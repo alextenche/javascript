0b9de6f09414c610ba31851e89751106e7b2e4b9
lexucuflexu <alex.tenche@gmail.com>
2014-10-02 10:35:50 +0300

    wiley

---
+++ Wiley/l8.js
@@ -0,0 +1,17 @@
+function Person(firstName, lastName){
+	this.firstName = firstName;
+	this.lastName = lastName;
+
+	this.getFullName = function(){
+		return this.firstName + " " + this.lastName;
+	};
+	
+	this.greet = function(person){
+		alert("Hello, " + person.getFullName());
+	};
+}
+
+var person1 = new Person("Jeremy","McPeak");
+var person2 = new Person("John","Doe");
+
+person2.greet(person1);
