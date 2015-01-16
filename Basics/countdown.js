36f9717dfbaa84dee11d20fa12719672b9331e9b
lexucuflexu <alex.tenche@gmail.com>
2015-01-17 01:27:55 +0200

    getting even more organized

--- Basics/countdown.js
+++ Basics/countdown.js
@@ -1,6 +1,6 @@
 var app = {};
 
-app.countdown = function(settings){
+app.countdown = function(settings, callback){
 	var interval;
 	var counter = 0;
 
@@ -11,6 +11,36 @@ app.countdown = function(settings){
 	if(settings === undefined){
 		console.log('Please provide settings');
 	} else {
-		console.log('ok');
+		if(settings.startAt === undefined || settings.endAt === undefined){
+			console.log('Start and end values are required');
+		} else {
+			startAt = parseInt(settings.startAt, 10);
+			endAt = parseInt(settings.endAt, 10);
+
+			if(!isNaN(startAt) && !isNaN(endAt)){
+				counter = startAt;
+
+				interval = setInterval(function(){
+					
+					// is the counter at the end
+					if(counter < endAt){
+						counter = 0;
+						startAt = 0;
+						endAt = 0;
+
+						if(callback !== undefined){
+							callback();
+						}
+
+						clearInterval(interval);
+					} else {
+						console.log(counter);
+					}
+					counter = counter - 1;
+
+					
+				}, 1000);
+			}
+		}
 	}
 };
