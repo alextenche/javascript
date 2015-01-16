3a1e7f83badf7844eb9095a3b761c0867b737816
lexucuflexu <alex.tenche@gmail.com>
2015-01-16 21:29:18 +0200

    started work on basics/countdown

---
+++ Basics/countdown.js
@@ -0,0 +1,16 @@
+var app = {};
+
+app.countdown = function(settings){
+	var interval;
+	var counter = 0;
+
+	// start and end points
+	startAt = 0;
+	endAt = 0;
+
+	if(settings === undefined){
+		console.log('Please provide settings');
+	} else {
+		console.log('ok');
+	}
+};
