470d4ca1da4df0531f9925f3145d54aee1323fd3
Alex Tenche <alex.tenche@gmail.com>
2015-11-25 21:58:52 +0200

    refactored Dice, added Calculator

--- Dice/dice.js
+++ Dice/dice.js
@@ -1,9 +1,14 @@
-"use strict";
-
-var dice = {
-  roll: function (){
-    var sides = 6;
-    var randomNumber = Math.floor(Math.random() * sides) + 1;
-    console.log(randomNumber);
-  }
+function diceRoll(){
+  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  return randomNumber;
 }
+
+function Dice(sides){
+  this.sides = sides;
+  this.roll: diceRoll
+}
+
+var dice = new Dice(6);
+var dice10 = new Dice(10);
+
+console.log(dice.roll === dice10.roll);
