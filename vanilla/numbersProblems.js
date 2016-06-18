36bd74b8906b211f2df765774aef384701d766e8
Alex Tenche <alex.tenche@gmail.com>
2016-06-18 09:30:33 +0300

    more numbers nonsense

--- vanilla/numbersProblems.js
+++ vanilla/numbersProblems.js
@@ -12,3 +12,32 @@ function tax (price, percent){
 }
 
 console.log(tax(9.85, 7.5));
+
+var mailedGlove = 9.85;
+var armotTax = 7.5;
+var total = mailedGlove + tax(mailedGlove, armotTax);
+
+console.log(parseInt("88"));
+console.log(parseInt("88 elephants"));
+console.log(parseInt("route 88"));
+
+console.log(parseFloat("88.88 elephants"));
+console.log(parseInt("more to go 88.88"));
+
+// problems with older browsers - thinks number is in octal
+var userAge = "021";
+console.log(parseInt(userAge));
+console.log(parseInt(userAge, 10));
+
+// number !
+console.log(typeof NaN);
+
+// false wow !
+console.log(NaN === NaN);
+
+// false - string
+isNaN("42");
+
+function isThisActuallyANumberDontLie(data){
+  return (typeof data === "number" && !isNaN(data));
+}
