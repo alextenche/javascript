502852f0e35236060898c7f707a869e2e3a47358
Alex Tenche <alex.tenche@gmail.com>
2016-06-21 21:08:26 +0300

    added augumentation

---
+++ vanilla/augumentation/armory.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var ARMORY = (function(war){
+  var weaponList: ["sword", "axe", "warhammer"];
+  var armorList: ["leather", "studded leather"];
+  var removeWeapon: function(){};
+  var replaceWeapon: function(){};
+  var removeArmor: function(){};
+  var replaceArmor: function(){};
+
+  return {
+    makeWeaponRequest: function(){
+      if(war) {
+        // let civilians have weaponry
+      }
+    },
+    makeArmorRequest: function(){}
+  };
+
+})(wartime);
---
+++ vanilla/augumentation/warchest.js
@@ -0,0 +1,10 @@
+"use strict";
+
+ARMORY = (function( oldNS){
+  var oilBarrels = 100;
+  var catapults = ["small", "big"];
+  oldNS.assignCatapult = function(regiment){
+    // assign a catapult and some oil barrels to a regiment
+  };
+  return oldNS;
+})( ARMORY );
