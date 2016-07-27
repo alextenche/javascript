b8602a6d212582001800f9c4d61c1faef9f81baa
Alexandru Tenche <alex.tenche@gmail.com>
2016-07-27 16:01:59 +0300

    started booleans

---
+++ vanilla/booleans/teams.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var teams = {
+  soccer: "PoliTimisoara",
+  hockey: "PoliOnIceTimisoara"
+};
+
+function keyChecker(){
+  if(teams.baskeball === undefined){
+    console.log('baskeball is undefined');
+  }
+}
