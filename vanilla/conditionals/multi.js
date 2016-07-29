c63e9f4c7c35fbeae75cd1765a253949b9b10e6d
Alex Tenche <alex.tenche@gmail.com>
2016-07-29 21:25:20 +0300

    basics conditionals

---
+++ vanilla/conditionals/multi.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var day = 1;
+
+switch(day){
+  case 0:
+    console.log('Sunday');
+    break;
+  case 1:
+    console.log('Monday');
+    break;
+  default:
+  console.log('hmm, weird..');
+}
---
+++ vanilla/conditionals/ternaryOperator.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var isTrue = true;
+
+isTrue ? console.log('is true') : console.log('is false');
+
+var yesOrNo = isTrue ? 'yes' : 'no';
+
+console.log(yesOrNo);
