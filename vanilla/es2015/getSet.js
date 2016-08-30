9f8c2c5ba51d0f7e640986815551b4948c8c27f5
Alex Tenche <alex.tenche@gmail.com>
2016-08-30 20:11:59 +0300

    finished getSet

--- vanilla/es2015/getSet.js
+++ vanilla/es2015/getSet.js
@@ -1,6 +1,17 @@
 'use strict';
 
 class Student {
+
+  get name () {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set name (input) {
+    let name = input.split(' ');
+    this.firstName = name[0];
+    this.lastName = name[1];
+  }
+
   constructor ({ firstName, lastName, age, intrestLevel = 5 } = {}) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -9,3 +20,10 @@ class Student {
     this.grades = new Map();
   }
 }
+
+let alexT = new Student({ firstName: 'Alexandru', lastName: 'Tenche', age: 38 });
+
+console.log(alexT.name);
+
+alexT.name = 'Alex Tenche';
+console.log(alexT.name);
