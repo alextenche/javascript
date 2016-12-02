ff7e2f8d415d41c86495d6ee052d5407d170fb2c
Alexandru Tenche <alex.tenche@gmail.com>
2016-12-02 22:47:20 +0200

    cleanup in vanilla

--- vanilla/es2015/letKeyword.js
+++ vanilla/es2015/letKeyword.js
@@ -2,9 +2,26 @@
 
 const person = {
     name: "Alex Tenche",
-    role: "student"
+    role: "js entuziast"
 }
 
-function personDescription() {
-    
+function personDescription(person) {
+    let description = person.name;
+    if (person.role) {
+      description += " is a ";
+      description += person.role;
+    }
+    console.log(description);
+}
+
+personDescription(person);
+
+
+const buttons = ['button 1', 'button 2', 'button 3', 'button 4', 'button 5', 'button 6', 'button 7', 'button 8', 'button 9'];
+
+var i = 5;
+
+for (let i = 0; i < buttons.length; i += 1) {
+  const button = buttons[i];
+  console.log(button);
 }
